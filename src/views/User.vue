cb00c2845cf02b4ef3e592446786650e82f47e2b
jennywongbc <jenwongbc@gmail.com>
2020-06-08 14:52:09 -0700

    travel app

---
+++ src/views/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Welcome {{ user }}</h1>
+
+    <router-link
+      :to="{
+        name: 'invoices'
+      }"
+    >
+    </router-link>
+    <button @click="logOut" class="btn">Log out</button>
+  </div>
+</template>
+<script>
+import store from "@/store.js";
+export default {
+  data() {
+    return {
+      user: store.user
+    };
+  },
+  methods: {
+    logOut() {
+      store.user = null;
+      this.$router.push("/");
+    }
+  }
+};
+</script>
